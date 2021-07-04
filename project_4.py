0192dd166f228f5970f5105e4d788da851a18e74
Surendra <kumawatanish0001@gmail.com>
2021-07-04 19:51:27 +0530

    don

---
+++ project_4.py
@@ -0,0 +1,16 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Celsius to Fahrenheit
+
+
+#You are making a Celsius to Fahrenheit converter.
+#Write a function to take the Celsius value as an argument and return the corresponding Fahrenheit value.
+>>> 
+>>> celsius = int(input())
+
+def conv(celsius):
+    print(1.8*celsius+32)
+
+    
+    
+conv(celsius
